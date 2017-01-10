0d8a8069e45b08c2d565f93083a6acd41078fe1b
Kyhan Turner <kyhanpt@gmail.com>
2017-01-09 16:59:18 -0800

    render charts from ajax results

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -11,14 +11,12 @@ class SearchBar extends Component {
   }
 
   onInputChange(event){
-    console.log(event.target.value)
     this.setState({term: event.target.value})
   }
 
   onFormSubmit(event){
     event.preventDefault();
 
-    console.log(event.target.value)
     this.props.fetchWeather(this.state.term);
     this.setState({ term: '' });
   }
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
+
+  renderWeather(cityData){
+    const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
+
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+        <td><Chart data={temps} units="K" color="orange" /></td>
+        <td><Chart data={pressures} units="hPa" color="green" /></td>
+        <td><Chart data={humidities} units="%" color="black" /></td>
+      </tr>
+    )
+  }
+
   render() {
     return (
       <table className="table table-hover">
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (K)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          {this.props.weather.map(this.renderWeather)}
+        </tbody>
       </table>
     )
   }
